fe414c7bb8e908bb8a2aca86d3f4627029d7dc4a
ARITRA SARKAR <aritrasarkar2002@gmail.com>
2023-06-23 21:44:14 +0530

    Add files via upload

---
+++ constants/index.js
@@ -0,0 +1,88 @@
+export const exploreWorlds = [
+  {
+    id: 'world-1',
+    imgUrl: '/planet-01.png',
+    title: 'The Hogwarts',
+  },
+  {
+    id: 'world-2',
+    imgUrl: '/planet-02.png',
+    title: 'The Upside Down',
+  },
+  {
+    id: 'world-3',
+    imgUrl: '/planet-03.png',
+    title: 'Kadirojo Permai',
+  },
+  {
+    id: 'world-4',
+    imgUrl: '/planet-04.png',
+    title: 'Paradise Island',
+  },
+  {
+    id: 'world-5',
+    imgUrl: '/planet-05.png',
+    title: 'Hawkins Labs',
+  },
+];
+
+export const startingFeatures = [
+  'Find a world that suits you and you want to enter',
+  'Enter the world by reading basmalah to be safe',
+  'No need to beat around the bush, just stay on the gas and have fun',
+];
+
+export const newFeatures = [
+  {
+    imgUrl: '/vrpano.svg',
+    title: 'A new world',
+    subtitle:
+        'we have the latest update with new world for you to try never mind',
+  },
+  {
+    imgUrl: '/headset.svg',
+    title: 'More realistic',
+    subtitle:
+        'In the latest update, your eyes are narrow, making the world more realistic than ever',
+  },
+];
+
+export const insights = [
+  {
+    imgUrl: '/planet-06.png',
+    title: 'Expansive Digital Universe',
+    subtitle:
+        'The metaverse represents a vast digital universe where users can explore, interact, and create in a boundless virtual space.',
+  },
+  {
+    imgUrl: '/planet-07.png',
+    title: 'Social Interaction and Collaboration',
+    subtitle:
+        'One of the key aspects of the metaverse is its focus on social interaction and collaboration.',
+  },
+  {
+    imgUrl: '/planet-08.png',
+    title: 'Diverse Experiences and Possibilities',
+    subtitle:
+        'The metaverse offers a wide range of experiences and possibilities for users. It serves as a platform for entertainment, gaming, education, commerce, art, and more.',
+  },
+];
+
+export const socials = [
+  {
+    name: 'twitter',
+    url: '/twitter.svg',
+  },
+  {
+    name: 'linkedin',
+    url: '/linkedin.svg',
+  },
+  {
+    name: 'instagram',
+    url: '/instagram.svg',
+  },
+  {
+    name: 'facebook',
+    url: '/facebook.svg',
+  },
+];
